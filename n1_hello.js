1736630558b76cbbd6c293f4fd571442ad421610
DuYang <Yangkeloff@foxmail.com>
2017-01-05 00:25:39 +0800

    no message

--- n1_hello.js
+++ n1_hello.js
@@ -3,7 +3,7 @@ http.createServer(function  (request,  response)  {
     response.writeHead(200,  {'Content-Type':  'text/html;  charset=utf-8'});  
     if(request.url!=="/favicon.ico"){  //清除第2此访问  
         console.log('访问');  
-        response.write('hello,world');  
+        response.write('hello,world. GOOD LUCK!');  
         response.end('hell,世界');//不写则没有http协议尾,但写了会产生两次访问  
     }  
 }).listen(8000);  
